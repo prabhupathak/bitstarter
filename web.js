0a083ec90004040a48f72ff41b942b64eacd7287
Prabhu pathak <prabhu.pathak@gmail.com>
2013-07-21 12:02:26 +0000

    commited on 21 july

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 
+var data = fs.readFileSync('index.html');
+
+var strdata= data.toStrng();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(strdata);
 });
 
 var port = process.env.PORT || 5000;
